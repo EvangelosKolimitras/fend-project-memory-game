a2f19d31c4e98f1b66c7fd7db453332668e5450a
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2018-06-04 23:16:33 +0200

    event listener & for-loop for handling onclick events for every card

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
+let card = document.querySelectorAll("card");
+let cards = [...card];
 
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", cardListener);
+}
+
+function cardListener() {
+    let cardStates = ["open", "show", "disabled"];
+    for(let i = 0; i < cardStates.length; i++) {    
+        this[i].classList.toggle([i]);
+    }
+}
 
 /*
  * Display the cards on the page
@@ -12,7 +24,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);

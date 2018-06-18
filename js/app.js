fe5e978755dee511dd5253e7007e5f3fec88efe5
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2018-06-18 21:57:45 +0200

    1. Commit(#c18bd3) with the time interval bug fixed 2. Comments properly added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-/* * Create a list that holds all of your cards */
-let card = document.querySelectorAll(".card");
-let cards = [...card];
-let flipped_cards = [];
+/* Create a list that holds all of your cards */
 
+/*
+    Elements retrieved from the DOM and variable assignments
+*/
+let card = document.querySelectorAll(".card");
 const board = document.querySelector("#deck");
 let matchedCard = document.getElementsByClassName("match");
 let restart_btn = document.querySelector(".restart");
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length,
+/*
+    Manipulation of retrieved elements 
+*/
+
+let cards = [...card];
+let flipped_cards = [];
+
+
+/*
+  FUNCTION-01: 
+  Shuffle function from http://stackoverflow.com/a/2450976
+*/
+
+ function shuffle(array) {
+    let currentIndex = array.length,
         temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -22,22 +35,33 @@ function shuffle(array) {
     return array;
 }
 
-// Add event listener onclick
+/*
+    Add event listeners onclick for every card
+*/
+
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", cardListener);
     cards[i].addEventListener("click", flip_card);
     cards[i].addEventListener("click", gameEnd);
 }
 
-// Different states on click
+/*
+    FUNCTION-02:
+    Different states on click
+*/
+
 function cardListener() {
     this.classList.toggle("open");
     this.classList.toggle("show");
     this.classList.toggle("disabled");
 }
 
-// Generates a new board and shuffles the deck
-function newGame() {
+/*
+    FUNCTION-03:
+    Generates a new board and shuffles the deck
+*/
+
+    function newGame() {
     let shuffledCards = shuffle(cards);
     for (let i = 0; i < shuffledCards.length; i++) {
         [].forEach.call(shuffledCards, function (x) {
@@ -46,6 +70,11 @@ function newGame() {
         cards[i].classList.remove("show", "open", "match", "disabled");
     }
 }
+
+/*
+    FUNCTION-04:
+    Add fipped styling on the card elements
+*/
 
 function flip_card() {
     flipped_cards.push(this);
@@ -60,6 +89,11 @@ function flip_card() {
     }
 }
 
+/*
+    FUNCTION-05:
+    Action if 2 flipped cards are making a match
+*/
+
 function cards_matched() {
     console.log('matched');
     flipped_cards[0].classList.add("match", "disabled");
@@ -68,6 +102,11 @@ function cards_matched() {
     flipped_cards[1].classList.remove("show", "open");
     flipped_cards = [];
 }
+
+/*
+    FUNCTION-06:
+    Action if 2 flipped cards are not making a match
+*/
 
 function cards_unmatched() {
     console.log('unmatched');
@@ -78,62 +117,83 @@ function cards_unmatched() {
     setTimeout(function () {
         flipped_cards[0].classList.remove("show", "open", "unmatched");
         flipped_cards[1].classList.remove("show", "open", "unmatched");
-        // enable();
+        enable();
         flipped_cards = [];
     }, 500);
 }
 
-function disable(){
-    Array.prototype.filter.call(cards, function(card){
+/*
+    FUNCTION-07:
+    adds the disabled class to prevent double clicking on the same elemet
+*/
+
+function disable() {
+    Array.prototype.filter.call(cards, function (card) {
         card.classList.add('disabled');
     });
 }
 
-function enable(){
-    Array.prototype.filter.call(cards, function(card){
+/*
+    FUNCTION-08:
+    adds the enabled class to a card in case an unmatch occurs
+*/
+
+function enable() {
+    Array.prototype.filter.call(cards, function (card) {
         card.classList.remove('disabled');
-        for(let i = 0; i < matchedCard.length; i++){
+        for (let i = 0; i < matchedCard.length; i++) {
             matchedCard[i].classList.add("disabled");
         }
     });
 }
 
+/*
+    FUNCTION-09:
+    Checks if all cards are flipped
+*/
+
 function gameEnd() {
-    if(matchedCard.length == 16) {
+    if (matchedCard.length == 16) {
         console.log("Congratulations!!");
         congratsMsg();
     }
 }
 
 /*
-
-    The starTimer() function is being invoked whene a user clicks the first card.
-
+    FUNCTION-10:
+    Automatically generates a timer of users first click
 */
-let s = 0;
-let m = 0; //s = seconds, m = minutes
-let h = 0;
-let time;
+
+let s = 0; // Seconds
+let m = 0; // Minutes
+let h = 0; // Hours
+
+let time; // Holds the interval function
+let timer = document.createElement("span"); //Creates the display timer element
 
 function startTimer() {
-    let timer = document.createElement("span");
-    time = setInterval(function() {
-
+    time = setInterval(function () {
         timer.innerHTML = h + " : " + m + " : " + s;
         restart_btn.insertBefore(timer, restart_btn.childNodes[0]);
         s++;
-        if(s == 60) {
-            
+        if (s == 60) {
             m++;
             s = 0;
         }
-        if(m == 60) {
+        if (m == 60) {
             h++;
             m = 0;
         }
-        
     }, 1000);
-    
+}
+
+/*
+    FUNCTION-11:
+    A congratulations message along with the resulting score appears to the user.
+*/
+
+function congratsMsg() {
+    console.log("Congratulations");    
 }
 
 document.body.onload = newGame();
